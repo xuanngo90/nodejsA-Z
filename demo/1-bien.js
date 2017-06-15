26f76ca272ca0a6eb38f9bf1349a1ac5fbdf6931
xuannd6620 <xuannd6620@co-well.vn>
2017-06-15 13:25:31 +0700

    update code

---
+++ demo/1-bien.js
@@ -0,0 +1,6 @@
+var s = 100;
+for (var i = 0; i < s; i++) {
+    if (i % 5 == 4) {
+        console.log(i);
+    }
+}
---
+++ demo/2-array.js
@@ -0,0 +1,11 @@
+var mang = ['cam', 'xoai', 'chuoi'];
+
+mang.push("man");
+
+var dodaimang = mang.length;
+
+console.log(dodaimang);
+
+for (var i = 0; i < dodaimang; i++) {
+    console.log(mang[i]);
+}
